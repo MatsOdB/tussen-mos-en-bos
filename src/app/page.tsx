36d0e703162c9da83eafbe867c35eb04e33ea39d
Mats Op de Beeck <matsopdebeeck@icloud.com>
2025-05-01 11:24:45 +0200

    Added sponsors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,60 @@ const navigation = [
   },
 ];
 
+const sponsors = [
+  {
+    name: "4 Events",
+    href: "https://www.4-events.be",
+    src: "/assets/img/sponsors/4events.jpeg",
+  },
+  {
+    name: "Asekuro",
+    href: "https://asekuro.be/",
+    src: "/assets/img/sponsors/asekuro.jpeg",
+  },
+  {
+    name: "Cools - De Cat",
+    href: "https://www.kantoorcdc.be/nl-be",
+    src: "/assets/img/sponsors/cdc.jpeg",
+  },
+  {
+    name: "Energy 4 Business",
+    href: "https://energy-4-business.webflow.io/",
+    src: "/assets/img/sponsors/energy4business.jpeg",
+  },
+  {
+    name: "GWN",
+    href: "#",
+    src: "/assets/img/sponsors/gwn.jpeg",
+  },
+  {
+    name: "Lardenoit",
+    href: "https://www.kantoorlardenoit.be/nl-be",
+    src: "/assets/img/sponsors/lardenoit.jpeg",
+  },
+  {
+    name: "Lovi",
+    href: "https://www.lovi.be/",
+    src: "/assets/img/sponsors/lovi.jpeg",
+  },
+  {
+    name: "Patrick De Ruisseaux",
+    href: "https://www.zakenkantoor-deruisseaux.be/nl-be",
+    src: "/assets/img/sponsors/pdr.jpeg",
+  },
+  {
+    name: "Pleegzorg",
+    href: "https://www.pleegzorg.be/antwerpen",
+    src: "/assets/img/sponsors/pleegzorg.jpeg",
+  },
+];
+
 export default function Home() {
   return (
     <>
       <NavBar navigation={navigation} className="font-alice" />
       <div className="h-[calc(100svh-65px)] overflow-y-scroll">
-        <section className="h-[calc(100svh-65px)] flex items-center justify-center">
+        <section className="h-[calc(100svh-65px)] items-center justify-center">
           <div className="bg-[url('/assets/img/header/01.png')] bg-center bg-cover h-full w-full">
             <div className="bg-orange-50 bg-opacity-60 h-full w-full">
               <div className="h-full flex lg:items-end items-center justify-center">
@@ -88,8 +136,8 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="h-[calc(100svh-65px)] flex items-center justify-center">
-          <div className="size-full flex flex-col lg:flex-row">
+        <section className="items-center justify-center">
+          <div className="size-full flex flex-col lg:flex-row mt-12">
             <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center">
               <Image
                 src={"/assets/img/content/sami.png"}
@@ -99,10 +147,10 @@ export default function Home() {
                 className="size-3/4 3xl:size-3/5 rounded-xl object-cover"
               />
             </div>
-            <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center">
+            <div className="h-1/2 lg:h-auto lg:w-1/2 justify-center items-center">
               <div className="size-full w-11/12 sm:w-3/4 lg:w-11/12 xl:w-3/4 lg:h-3/4 3xl:size-3/5 flex justify-center lg:items-center">
                 <div>
-                  <h1 className="text-center font-veryberry text-3xl sm:text-5xl 2xl:text-7xl 3xl:text-8xl text-cyan-700">
+                  <h1 className="text-center font-veryberry text-3xl sm:text-5xl 2xl:text-7xl 3xl:text-8xl text-cyan-700 mt-12">
                     Hey Speurneus!
                   </h1>
                   <p className="text-center font-alice text-sm sm:text-xl 3xl:text-2xl 4xl:text-3xl sm:mt-12 xl:text-2xl">
@@ -124,7 +172,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="h-[calc(100svh-65px)] flex items-center justify-center mt-32 sm:mt-auto">
+        <section className="items-center justify-center mt-32 sm:mt-auto">
           <div className="size-full flex items-center justify-center">
             <div className="flex flex-col items-center 2xl:w-3/4 4xl:w-3/5">
               <h1 className="text-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl 3xl:text-9xl font-veryberry text-orange-400">
@@ -143,12 +191,12 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="h-[calc(100svh-65px)] flex items-center justify-center mt-16 sm:mt-auto">
+        <section className="items-center justify-center mt-16 sm:mt-auto">
           <div className="size-full bg-custom_green">
             <h1 className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl 3xl:text-9xl font-veryberry text-custom_rose mt-8 xl:mt-16 2xl:mt-24">
               Ontmoet mijn vriendjes!
             </h1>
-            <div className="w-full h-max flex items-center justify-center">
+            <div className="w-full h-max flex items-center justify-center pb-6">
               <div className="w-4/5 xl:w-3/4 mt-16 xl:mt-24 2xl:mt-32 grid grid-cols-2 lg:grid-cols-3 lg:gap-y-16 gap-y-6 sm:gap-y-24">
                 <div className="flex justify-center items-center">
                   <div className="rounded-full size-28 sm:size-36 md:size-44 lg:size-48 xl:size-56 2xl:size-64 4xl:size-96 bg-[url('/assets/img/content/12.png')] bg-center bg-cover group">
@@ -208,7 +256,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="h-[calc(100svh-65px)] flex items-center justify-center mt-16 sm:mt-auto">
+        <section className="items-center justify-center mt-16 sm:mt-auto">
           <div className="size-full flex flex-col lg:flex-row">
             <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center">
               <div className="size-full w-11/12 sm:w-3/4 lg:w-11/12 xl:w-3/4 lg:h-3/4 3xl:size-3/5 flex justify-center items-center">
@@ -220,13 +268,13 @@ export default function Home() {
                     Het Sint-Annabos is een magische plek, vol oude bomen die fluisteren en takken
                     die zachtjes bewegen in de wind. Mijn huis is een holle boom, lekker knus en
                     gezellig. Het is een plek vol magie en geheimen, en ik voel me er helemaal
-                    thuis. Kom jij mijn thuis bezoeken op 2 - 3 mei? Wie weet ontdek jij wel een geheime
-                    plek die zelfs ik nog niet ken!
+                    thuis. Kom jij mijn thuis bezoeken op 2 - 3 mei? Wie weet ontdek jij wel een
+                    geheime plek die zelfs ik nog niet ken!
                   </p>
                 </div>
               </div>
             </div>
-            <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center">
+            <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center mt-12">
               <Image
                 src={"/assets/img/content/magische thuis.png"}
                 alt="Sami's thuis"
@@ -237,9 +285,9 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="md:snap-start h-[calc(100svh-65px)] sm:min-h-[60rem] md:min-h-0 flex items-center justify-center mt-16 sm:mt-auto">
+        <section className="items-center justify-center mt-16 sm:mt-auto">
           <div className="size-full flex flex-col lg:flex-row bg-custom_green">
-            <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center">
+            <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center mt-12">
               <Image
                 src={"/assets/img/content/pannenkoek.jpg"}
                 alt="Pannenkoeken"
@@ -248,7 +296,7 @@ export default function Home() {
                 className="size-3/4 3xl:size-3/5 rounded-xl object-cover"
               />
             </div>
-            <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center">
+            <div className="h-1/2 lg:h-auto lg:w-1/2 flex justify-center items-center mb-12">
               <div className="size-full w-11/12 sm:w-3/4 lg:w-11/12 xl:w-3/4 lg:h-3/4 3xl:size-3/5 flex justify-center lg:items-center">
                 <div>
                   <h1 className="text-center font-veryberry text-3xl sm:text-5xl 2xl:text-7xl 3xl:text-8xl text-custom_rose">
@@ -260,6 +308,28 @@ export default function Home() {
                     en lekker smikkelen van heerlijke pannenkoeken!
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="items-center justify-center mt-16">
+          <div className="size-full flex items-center justify-center">
+            <div className="flex flex-col items-center 2xl:w-3/4 4xl:w-3/5">
+              <h1 className="text-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl 3xl:text-9xl font-veryberry text-custom_indigo">
+                Trotse sponsors
+              </h1>
+              <div className="flex flex-wrap justify-center items-center gap-6 my-12">
+                {sponsors.map((sponsor) => (
+                  <a
+                    key={sponsor.name}
+                    href={sponsor.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="relative h-32 w-32 opacity-90 hover:opacity-100 transition-opacity"
+                  >
+                    <Image src={sponsor.src} alt={sponsor.name} layout="fill" objectFit="contain" />
+                  </a>
+                ))}
               </div>
             </div>
           </div>
